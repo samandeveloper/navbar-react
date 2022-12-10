fa53e124b85fc76ec99b635ee4369f65eb3cb4af
Saman M. Mohaghegh <sn.mohaghegh@gmail.com>
2022-12-10 00:47:59 -0700

    Updates

---
+++ precache-manifest.2c818a87b36f812b149279b228474e16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b612fe6fa03c8894cc67d28435ed24",
+    "url": "/navbar-react/index.html"
+  },
+  {
+    "revision": "e42896f5d86173f6b8e1",
+    "url": "/navbar-react/static/css/main.61321318.chunk.css"
+  },
+  {
+    "revision": "5036664f6a0cc62b8b04",
+    "url": "/navbar-react/static/js/2.41d20619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/navbar-react/static/js/2.41d20619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42896f5d86173f6b8e1",
+    "url": "/navbar-react/static/js/main.a7420273.chunk.js"
+  },
+  {
+    "revision": "c8fed290e03d9489f9d1",
+    "url": "/navbar-react/static/js/runtime-main.d7da5607.js"
+  },
+  {
+    "revision": "2b686f2e5682570fb85fc952a4581419",
+    "url": "/navbar-react/static/media/logo.2b686f2e.svg"
+  }
+]);
